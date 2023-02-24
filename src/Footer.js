dced9a86c48a29df3fcf471c574d7986986e6df9
jainil15 <79244804+jainil15@users.noreply.github.com>
2023-02-24 14:48:06 +0000

    border

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,15 @@
 export default function Footer() {
     return (
-        
+
             <div className="footer">
+                <div className="footer-icons">
                 <a href="https://www.facebook.com"><img src="https://cdn1.iconfinder.com/data/icons/social-media-rounded-corners/512/Rounded_Facebook_svg-1024.png"></img></a>
                 <a href="https://in.linkedin.com"><img src="https://cdn1.iconfinder.com/data/icons/social-media-rounded-corners/512/Rounded_Linkedin2_svg-1024.png"></img></a>
                 <a href="https://www.instagram.com"><img src="https://cdn1.iconfinder.com/data/icons/social-media-rounded-corners/512/Rounded_Instagram_svg-1024.png"></img></a>
                 <a href="https://www.twitter.com"><img src="https://cdn1.iconfinder.com/data/icons/social-media-rounded-corners/512/Rounded_Twitter5_svg-1024.png"></img></a>
             </div>
+            </div>
+            
+
     );
 }
--- src/Main.js
+++ src/Main.js
@@ -8,14 +8,14 @@ export default function Main() {
                 <small>2 months</small>
             </div>
             <div className="middle--buttons">
-                <a href="https://in.linkedin.com/">Linkden</a>
-                <a href="https://in.linkedin.com/">Email</a>
+                <a href="https://in.linkedin.com/"><img src="https://cdn-icons-png.flaticon.com/512/49/49656.png"/>Linkden</a>
+                <a href="https://in.linkedin.com/"><img src="https://cdn-icons-png.flaticon.com/512/666/666162.png"/>Email</a>
             </div>
             <div className="about">
                 <h2>About</h2>
                 <small>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</small></div>
             <div className="about">
-                <h2>Interests</h2>
+                <h2>Intrests</h2>
                 <small>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here',</small></div>
         </div>
 
